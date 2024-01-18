4758cfed8a77cedbe8b530faf2e66d78f0c5ebf6
Lori-Fu <124735319+Lori-Fu@users.noreply.github.com>
2024-01-17 23:15:59 -0500

    init

---
+++ article/src/main/java/com/trip/article/ArticleApplication.java
@@ -0,0 +1,13 @@
+package com.trip.article;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArticleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArticleApplication.class, args);
+    }
+
+}
---
+++ user/src/test/java/com/trip/user/UserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trip.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
